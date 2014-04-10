e4f2adf5d616b8af5200b6fd8065c30000e352c2
porange <psalmon.ext@orange.com>
2014-04-10 10:29:07 +0100

    Comment FILO : the good way (part 2)

--- client/app/scripts/modules/production/collections/comments.js
+++ client/app/scripts/modules/production/collections/comments.js
@@ -3,7 +3,6 @@ define(function (require) {
     "use strict";
 
     var Backbone = require('backbone');
-
     var Comment = require('modules/production/models/comment');
 
     var CommentCollection = Backbone.Collection.extend({
