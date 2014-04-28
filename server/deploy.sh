a193e1f7319e3d94b8b5f2acef6480368e970943
Guillaume <guillaume@warnode.(none)>
2014-04-28 08:24:26 -0400

    updates deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,7 +3,7 @@ git checkout .;
 git pull;
 
 # Restart server (production + dev)
-forever stop app.js;
-sudo NODE_ENV=production forever -a -l node.log -o node.log -e node.log start app.js;
-sudo NODE_ENV=development forever -a -l node-dev.log -o node-dev.log -e node-dev.log start app.js;
+sudo forever stop jam_prod jam_dev;
+sudo NODE_ENV=production forever --uid "jam_prod" -a -l node.log -o node.log -e node.log start app.js;
+sudo NODE_ENV=development forever --uid "jam_dev" -a -l node-dev.log -o node-dev.log -e node-dev.log start app.js;
 
