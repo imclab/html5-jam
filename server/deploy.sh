964dcd04f33d08811aad4043888a70927beadf44
Guillaume <guillaume@warnode.(none)>
2014-04-28 06:15:13 -0400

    updates deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -4,4 +4,4 @@ git pull;
 
 # Restart server
 forever stop app.js;
-sudo forever -a -l node.log -o node.log -e node.log IS_PROD=1 start app.js;
+sudo forever -a -l node.log -o node.log -e node.log start app.js;
